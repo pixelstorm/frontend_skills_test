3794c9015c6c6a8c55027df8bb787b8a2867d84b
Marga Satrya <margasatrya@gmail.com>
2019-09-04 15:14:31 +0700

    01. Add several files to help me to speed the project process including Gruntjs file.

---
+++ Gruntfile.js
@@ -0,0 +1,58 @@
+module.exports = function ( grunt ) {
+
+	// Load all Grunt tasks
+	require( 'jit-grunt' )( grunt );
+
+	const sass = require( 'node-sass' );
+
+	grunt.initConfig( {
+
+		pkg: grunt.file.readJSON( 'package.json' ),
+
+		// Compile our sass.
+		sass: {
+			prod: {
+				options: {
+					implementation: sass,
+					outputStyle: 'compressed'
+				},
+				files: {
+					'css/clean-blog.css': 'less/clean-blog.scss',
+				}
+			}
+		},
+
+		// Watch for changes.
+		watch: {
+			options: {
+				livereload: true,
+			},
+			scss: {
+				files: [ 'scss/**/*.scss' ],
+				tasks: [ 'sass:prod' ]
+			}
+		},
+
+		connect: {
+			server: {
+				options: {
+					base: '.',
+					livereload: true
+				}
+			}
+		},
+
+	} );
+
+	// Prepare
+	grunt.registerTask( '', [
+
+	] );
+
+	// Start the static server
+	grunt.registerTask( 'server', [
+		'connect',
+		'watch'
+	] );
+
+};
